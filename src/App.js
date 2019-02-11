b766b7d10e3250c08febae1c6f046f23f33b6e2c
Alexander Caroselli <alexandersylvia9@msn.com>
2019-02-11 13:52:04 -0700

    testing again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Test <code>src/App.js</code> and save to reload.
+            Banana <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
