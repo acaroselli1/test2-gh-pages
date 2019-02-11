ce5343d2c8cfc1c7484a405645db352037050ba7
Alexander Caroselli <alexandersylvia9@msn.com>
2019-02-11 13:51:30 -0700

    Updates

--- precache-manifest.93d9db2a0acbabfe3bc2cd743a3d6bf3.js
+++ precache-manifest.93d9db2a0acbabfe3bc2cd743a3d6bf3.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/test2-gh-pages/static/js/runtime~main.e05fa448.js"
   },
   {
-    "revision": "95c1dc0e8a0bd1dbf24e",
-    "url": "/test2-gh-pages/static/js/main.95c1dc0e.chunk.js"
+    "revision": "ba3adb856ffc521439f3",
+    "url": "/test2-gh-pages/static/js/main.ba3adb85.chunk.js"
   },
   {
     "revision": "d16a266844ec01aafc8a",
     "url": "/test2-gh-pages/static/js/2.d16a2668.chunk.js"
   },
   {
-    "revision": "95c1dc0e8a0bd1dbf24e",
+    "revision": "ba3adb856ffc521439f3",
     "url": "/test2-gh-pages/static/css/main.4266ad1a.chunk.css"
   },
   {
-    "revision": "b913bf56f205ac1f52ef5014e198e693",
+    "revision": "90d83ee222818ce44ba184a67af0f8af",
     "url": "/test2-gh-pages/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test2-gh-pages/precache-manifest.44be0a8c016ef67e29c028f1e6cbefb3.js"
+  "/test2-gh-pages/precache-manifest.93d9db2a0acbabfe3bc2cd743a3d6bf3.js"
 );
 
 workbox.clientsClaim();
